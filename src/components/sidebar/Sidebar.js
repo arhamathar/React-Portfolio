c7f0d3bddc60503e07a4c58f0f04532a712c0504
arhamathar <arhamathar789@gmail.com>
2021-02-03 00:53:21 +0530

    social contact added to sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,37 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Avatar from '../../assets/images/avatar.svg';
+import Github from '../../assets/images/github.svg';
+import Download from '../../assets/images/download.svg';
+import Linkedin from '../../assets/images/linkedin.svg';
+import Facebook from '../../assets/images/facebook.svg';
+import Instagram from '../../assets/images/instagram.svg';
+
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar-avatar" src={Avatar} alt="avatar" />
-            <h3>Arham <span>Athar</span></h3>
+            <h2>Arham <span>Athar</span></h2>
             <div>
                 <h4>Web Developer</h4>
-                <Link to="">
-                    <div>
-                        Download Resume
-                </div>
-                </Link>
             </div>
+            <a href="/">
+                <div className="info">
+                    <img src={Download} alt="download" />
+                        Download Resume
+                    </div>
+            </a>
             <a
                 href="https://github.com/arhamathar"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                Github
+                <div className="info">
+                    <img className="sidebar-icon" src={Github} alt="github" />
+                    <h4>Github</h4>
+                </div>
             </a>
             <div className="contact">
                 <p>Delhi, India</p>
-                <p>1234567890</p>
+                <p>123-456-7890</p>
                 <p>arhamathar789@gmail.com</p>
             </div>
             <div className="social-contact">
-                <a href="/">Facebook</a>
-                <a href="/">Instagram</a>
-                <a href="/">LinkedIn</a>
+                <a href="/">
+                    <img className="sidebar-icon" src={Facebook} alt="facebook" />
+                </a>
+                <a href="https://www.instagram.com/athararham/">
+                    <img className="sidebar-icon" src={Instagram} alt="instagram" />
+                </a>
+                <a href="https://www.linkedin.com/in/arham-athar-4b29bb1ab">
+                    <img className="sidebar-icon" src={Linkedin} alt="linkedin" />
+                </a>
             </div>
         </div>
     );
