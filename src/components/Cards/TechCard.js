d96e94c834d0d74c8861ee10c214004e93a20e19
arhamathar <arhamathar789@gmail.com>
2021-02-04 00:37:33 +0530

    resume skill structure complete

--- src/components/Cards/TechCard.js
+++ src/components/Cards/TechCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-function TechCard() {
+function TechCard(props) {
     return (
-        <div>
-
-        </div>
+        <article className="languages">
+            <h3>Languages</h3>
+            <div>
+                <div className="skill-info">
+                    <img className="skill-info__logo" src={props.Javascript} alt="js" />
+                    <p>Javascript</p>
+                </div>
+                <div className="skill-info">
+                    <img className="skill-info__logo" src={props.Python} alt="python" />
+                    <p>Python</p>
+                </div>
+                <div className="skill-info">
+                    <img className="skill-info__logo" src={props.Cpp} alt="c++" />
+                    <p>C++</p>
+                </div>
+            </div>
+        </article>
     );
 }
 
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,38 +5,61 @@ import Javascript from '../assets/images/js.svg';
 
 function Resume() {
     return (
-        // <div className="resume">
-        //     <div >
-        //         <div className="education"></div>
-        //         <div className="expirence"></div>
-        //     </div>
-        //     <div className="skills">
-        //         <article className="languages">
-        //             <h3>Languages</h3>
-        //             <div>
-        //                 <div className="skill-info">
-        //                     <img className= src={Javascript} alt="js" />
-        //                     <p>Javascript</p>
-        //                 </div>
-        //                 <div className="skill-info">
-        //                     <img className= src={Python} alt="python" />
-        //                     <p>Python</p>
-        //                 </div>
-        //                 <div className="skill-info">
-        //                     <img className= src={Cpp} alt="c++" />
-        //                     <p>C++</p>
-        //                 </div>
-        //             </div>
-        //         </article>
-        //         <article className="technologies">
-
-        //         </article>
-        //         <article className="tools">
-
-        //         </article>
-        //     </div>
-        // </div>
-        <div>Resume</div>
+        <div className="resume">
+            <div >
+                <div className="education"></div>
+                <div className="expirence"></div>
+            </div>
+            <div className="skills">
+                <article className="languages">
+                    <h3>Languages</h3>
+                    <div>
+                        <div className="skill-info">
+                            <img className="skill-info__logo" src={Javascript} alt="js" />
+                            <p>Javascript</p>
+                        </div>
+                        <div className="skill-info">
+                            <img className="skill-info__logo" src={Python} alt="python" />
+                            <p>Python</p>
+                        </div>
+                        <div className="skill-info">
+                            <img className="skill-info__logo" src={Cpp} alt="c++" />
+                            <p>C++</p>
+                        </div>
+                    </div>
+                </article>
+                <article className="technologies">
+                    <h3>Technologies</h3>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Javascript} alt="js" />
+                        <p>Javascript</p>
+                    </div>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Python} alt="python" />
+                        <p>Python</p>
+                    </div>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Cpp} alt="c++" />
+                        <p>C++</p>
+                    </div>
+                </article>
+                <article className="tools">
+                    <h3>Tools</h3>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Javascript} alt="js" />
+                        <p>Javascript</p>
+                    </div>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Python} alt="python" />
+                        <p>Python</p>
+                    </div>
+                    <div className="skill-info">
+                        <img className="skill-info__logo" src={Cpp} alt="c++" />
+                        <p>C++</p>
+                    </div>
+                </article>
+            </div>
+        </div>
     );
 }
 
