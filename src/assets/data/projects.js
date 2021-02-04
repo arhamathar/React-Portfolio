ad59e4cec068cc83883a51ba3f5d5987434fc559
arhamathar <arhamathar789@gmail.com>
2021-02-04 23:49:54 +0530

    projects data added

---
+++ src/assets/data/projects.js
@@ -0,0 +1,34 @@
+
+
+const projects = [
+    {
+        name: 'Weather App',
+        // image: Weather,
+        deployed_url: 'https://arham-weather.netlify.app/',
+        github_url: 'https://github.com/arhamathar/PWA-Weather',
+        category: ['react.js']
+    },
+    {
+        name: 'Notes Keeper',
+        // image: Notes,
+        deployed_url: 'https://arham-notes-keeper.netlify.app/',
+        github_url: 'https://github.com/Dey-Sumit/Dev-talks',
+        category: ['node.js', 'mongoDB', 'react.js']
+    },
+    {
+        name: 'Photogram',
+        // image: Photogram,
+        deployed_url: '',
+        github_url: '',
+        category: ['react.js', 'nodejs', 'mongoDB']
+    },
+    {
+        name: 'Burger Builder',
+        // image: Burger,
+        deployed_url: '',
+        github_url: '',
+        category: ['react.js', 'firebase']
+    },
+]
+
+export default projects;
