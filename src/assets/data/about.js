a6e247e39a31a035934a557dfde6ba0148f23b80
arhamathar <arhamathar789@gmail.com>
2021-02-04 23:56:32 +0530

    about page skill data added

---
+++ src/assets/data/about.js
@@ -0,0 +1,29 @@
+import Api from '../images/api.svg'
+import Puzzle from '../images/puzzle.svg';
+import Backend from '../images/repair.svg'
+import Frontend from '../images/computer.svg'
+
+const skills = [
+    {
+        icon: Frontend,
+        title: "Frontend Development",
+        content: "I can build beautiful SPA using HTML, CSS and React.js"
+    },
+    {
+        icon: Backend,
+        title: "Backend  Development",
+        content: "Handle NoSql database , Node-Express-server and Api using"
+    },
+    {
+        icon: Api,
+        title: "API Development",
+        content: "I can develop robust REST API using nodeJs & express"
+    },
+    {
+        icon: Puzzle,
+        title: "Competitive Coder",
+        content: "A daily problem solver in GeeksforGeeks"
+    }
+]
+
+export default skills;
