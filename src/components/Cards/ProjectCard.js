f3e2df6ca600633b6bf66f5d27afc106801d33f0
arhamathar <arhamathar789@gmail.com>
2021-02-06 13:22:13 +0530

    basic grid ui added for projects

--- src/components/Cards/ProjectCard.js
+++ src/components/Cards/ProjectCard.js
@@ -5,11 +5,11 @@ const ProjectCard = (props) => {
         <div className="project-card">
             <figure className="project-container">
                 <a href={props.project.deployed_url}>
-                    <img src={props.project.image} alt={props.project.name} />
+                    <img className="project-card__img" src={props.project.image} alt={props.project.name} />
                 </a>
                 <div className="project-info">
                     <h2>{props.project.name}</h2>
-                    <div>
+                    <div className="project-info__links">
                         <a href={props.project.github_url}>Repo Link</a>
                         <button>Detils</button>
                     </div>
