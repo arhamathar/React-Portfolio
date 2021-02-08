be86700f38bd63fb9233f22d562469ed0d60c106
arhamathar <arhamathar789@gmail.com>
2021-02-08 23:39:06 +0530

    link opening in new tab

--- src/components/Cards/ProjectCard.js
+++ src/components/Cards/ProjectCard.js
@@ -5,13 +5,21 @@ const ProjectCard = (props) => {
     return (
         <div className="project-card">
             <figure className="project-container">
-                <a href={props.project.deployed_url}>
+                <a
+                    href={props.project.deployed_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img className="project-card__img" src={props.project.image} alt={props.project.name} />
                 </a>
                 <div className="project-info">
                     <h3>{props.project.name}</h3>
                     <div className="project-info__links">
-                        <a href={props.project.github_url}>
+                        <a
+                            href={props.project.github_url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             <img src={Github} alt="github" />&nbsp;
                             <span>Repo Link</span>
                         </a>
