3e2c0c62d8a672e89b26f6cfc1336d52cec9e9df
arhamathar <arhamathar789@gmail.com>
2021-02-14 00:01:35 +0530

    ui of the modal improved

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,12 +5,12 @@ const Details = () => {
         <div className="details">
             <h2>Weather App</h2>
             <p>
-                <span>Description: </span>
+                <span className="info_name">Description:- </span><br />
                 This is a beautiful PWA weather app which uses service workers
                 to cache data and works even if the users is offline.
             </p>
             <p>
-                <span>Tech Used: </span>
+                <span className="info_name">Tech Used:- </span>
                 <li>Create React App</li>
                 <li>Open Weather Api</li>
                 <li>Service Workers</li>
--- src/components/UI/Backdrop.js
+++ src/components/UI/Backdrop.js
@@ -8,7 +8,8 @@ function Backdrop(props) {
         position: 'fixed',
         top: '0',
         left: '0',
-        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        background: 'linear-gradient(to right top, rgba(46, 191, 145, .95), rgba(131, 96, 195, .95))'
     }
     return (
         props.show && <div style={styleClass} onClick={props.close} />
